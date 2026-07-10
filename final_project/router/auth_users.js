c259007ba23df7231e4d96ea303b3c0fdf324298
Praveen M <136952628+Praveen-M-18@users.noreply.github.com>
2026-07-10 11:42:11 +0530

    Implement user validation in auth_users.js

---
+++ final_project/router/auth_users.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [];
+
+const isValid = (username)=>{ 
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return false;
+  } else {
+    return true;
+  }
+}
+
+module.exports = {
+  isValid: isValid,
+  users: users
+};
